d4e6f34a33bc9291e29140365e57f1383c1c6711
Steven N. Wilson <49218333+steven-n-wilson@users.noreply.github.com>
2025-05-05 12:40:29 -0400

    updates

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/steven-n-wilson.github.io/',
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        entryFileNames: `assets/[name].[hash].js`,
-        chunkFileNames: `assets/[name].[hash].js`,
-        assetFileNames: `assets/[name].[hash].[ext]`
-      }
-    }
+    outDir: 'dist'
   }
 })
