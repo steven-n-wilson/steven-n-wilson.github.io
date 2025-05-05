ba63152c1a4cf647ec0a389a6fe1bf74b920287b
Steven N. Wilson <49218333+steven-n-wilson@users.noreply.github.com>
2025-05-05 12:18:31 -0400

    Fix MIME type issue and update build configuration

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/steven-n-wilson.github.io/',
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name].[ext]',
+        chunkFileNames: 'assets/[name].[hash].js',
+        entryFileNames: 'assets/[name].[hash].js',
+      },
+    },
+  },
 })
